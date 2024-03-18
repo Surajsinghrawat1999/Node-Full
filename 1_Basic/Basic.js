b1e1731633a45c9ddb8ee6e4c3875a1834e62137
Suraj Rawat <surajsinghrawat1999@gmail.com>
2024-03-18 08:01:54 +0530

    first commit

---
+++ 1_Basic/Basic.js
@@ -0,0 +1,20 @@
+// console.log("Suraj");
+// var a = 20;
+// var b = 30;
+// var c = 40;
+// console.warn(a+b+c);
+
+// var x = 30;
+// if (x===30){
+//     console.log("Matched");
+// }
+
+
+// var x = 20;
+// for(i=0; i<10; i++){
+//     console.log(i);
+// }
+
+
+// const arr = [2,4,5,7,3];
+// console.log(arr[0])
---
+++ 3_create_fs/create_folder.js
@@ -0,0 +1,2 @@
+const fs = require('fs');
+fs.writeFileSync("Hello.txt", "Hey this is newly created file");
